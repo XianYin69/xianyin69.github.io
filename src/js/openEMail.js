1da0117428b0ea4d683f46efc471f431c1cd4184
Yan <62499077+XianYin69@users.noreply.github.com>
2025-11-03 21:06:03 +0800

    update

---
+++ src/js/openEMail.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const emailInfo = document.querySelectorAll("#eMail");
+    emailInfo.forEach(item => {
+        item.addEventListener("click", function() {
+            const email = this.getAttribute("data-email");
+            if (isValidEmail(email)) {
+                window.location.href = `mailto:${email}`;
+            } else {
+                alert("Error");
+            }
+        })
+    });
+
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+})
