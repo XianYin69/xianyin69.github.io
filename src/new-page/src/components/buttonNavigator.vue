fce6b9bed819ac4a56f5409d51c6ad81db67687d
Yan <62499077+XianYin69@users.noreply.github.com>
2025-12-09 20:16:36 +0800

    add a new component

---
+++ src/new-page/src/components/buttonNavigator.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+
+</template>
+    <div class="tips">
+        <p id="tip">更多</p>
+    </div>
+    <div class="mediumMess">
+        <p id = "XianYin69">github:XianYin69</p>
+        <p id = "www324608yf@outlook.com">电子邮箱：www324608yf@outlook.com</p>
+    </div>
+    <div class="rightTips">
+        <p id = "language">语言</p>
+    </div>
+<style>
+
+</style>
